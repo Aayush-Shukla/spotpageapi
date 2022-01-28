dc88e2b80fdfa4dca5f556db88fcf517ebd8cc43
Ayush Shukla <34869001+Aayush-Shukla@users.noreply.github.com>
2022-01-28 15:33:26 +0530

    add force dark mode

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,11 @@ import '../styles/global.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import Head from 'next/head'
 export default function App({ Component, pageProps }) {
+
   return  (
     <ChakraProvider>
+    
+    
   <Head>
       <title>@ayushittin.me</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
