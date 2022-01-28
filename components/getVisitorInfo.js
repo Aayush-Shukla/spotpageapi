5a18067cfc965a3bf49a090b0e55d075aaf79fd5
Ayush Shukla <34869001+Aayush-Shukla@users.noreply.github.com>
2022-01-28 16:02:06 +0530

    enable tele and mongo

--- components/getVisitorInfo.js
+++ components/getVisitorInfo.js
@@ -1,14 +1,14 @@
 import ipstack from "./ipstack"
 import notifyTelegram from "./notifyTelegram"
 var moment = require('moment-timezone')
-// const {
-//     MongoClient
-// } = require('mongodb');
-// const uri = process.env.DATABASE_URL
-// const client = new MongoClient(uri, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// });
+const {
+    MongoClient
+} = require('mongodb');
+const uri = process.env.DATABASE_URL
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 export default async function getVisitorInfo(req, data) {
     var visitorInfo = {
         ip: (req.headers['x-forwarded-for'] || '').split(',')[0] || req.connection.remoteAddress,
@@ -16,7 +16,7 @@ export default async function getVisitorInfo(req, data) {
         referer: req.headers.referer || 'NONE',
         time: moment().tz('Asia/Kolkata').format('hh:mmA DD-MM-YYYY')
     }
-    // client.connect()
+    client.connect()
     async function ipst() { //had to do this to fix the premature return of await function. Temp fix
         var connInfo = await ipstack(visitorInfo.ip, data)
         var info = {
@@ -25,10 +25,10 @@ export default async function getVisitorInfo(req, data) {
         }
         console.log("sent")
         notifyTelegram(info, data) // send notification to telegram
-        // const collection = client.db("spotpage").collection("visitor");
-        // collection.insertOne(info, function (err, res) { //inserting into mongodb for record
-        //     if (err) throw err;
-        // })
+        const collection = client.db("spotpage").collection("visitor");
+        collection.insertOne(info, function (err, res) { //inserting into mongodb for record
+            if (err) throw err;
+        })
     }
     ipst()
 }
